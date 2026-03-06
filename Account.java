2be66192b3f1cfde3e70f736b2028eaaeb5d7cc5
zimelemdluli603-png <zimelemdluli603@gmail.com>
2026-03-06 23:38:00 +0200

    Add files via upload
    Fixed naming conventions (getBalance, deposit).
    
    Added withdraw() with sufficient funds check.
    
    Added hasSufficientFunds() helper method.
    
    Added isValidAccountNumber() with regex validation.
    
    Overrode toString() for neat output.
    
    Added a driver program to test everything.

--- Account.java
+++ Account.java
@@ -1,46 +1,64 @@
+
 public class Account
 {
     private String accNumber;
     private double balance;
     
-    
     public Account()
     {
         this.accNumber = "";
-        this.balance = 0;
-       
+        this.balance = 0.0;
         
     }
-    public Account(String accNumber, double balance)
+
+    public Account(String aNo, double bal)
     {
-        this.accNumber = accNumber;
-        this.balance = balance;
-        
-        
+        this.accNumber = aNo;
+        this.balance = bal;
     }
-    public void setAccNumber(String accNumber)
+    public void setAccNumber(String aNo)
     {
-        this.accNumber = accNumber;
+        this.accNumber = aNo;
     }
-    public void setBalance(double bal)
+    public void setBalance(double bal) 
     {
         this.balance = bal;
     }
-    public String getAccNumber()
+    public String getAccNumber() 
     {
         return accNumber;
     }
-    public double getbalance()
+    public double getBalance()
     {
         return balance;
     }
-    public void deposite(double amount)
+    public void deposit(double amount) 
     {
-        if(amount > 0)
+        if (amount > 0) 
         {
             balance += amount;
         }
     }
+    public boolean withdraw(double amount) 
+    {
+        if (hasSufficientFunds(amount)) 
+        {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
     
+    public boolean hasSufficientFunds(double amount)
+    {
+        return balance >= amount;
+    }
+    public boolean isValidAccountNumber() 
+    {
+        return accNumber.matches("^[A-Z]{3}-\\d{6}$");
+    }
+    public String toString()
+    {
+        return "Account Number: " + accNumber + "\nBalance: R" + String.format("%.2f", balance);
+    }
 }
-
